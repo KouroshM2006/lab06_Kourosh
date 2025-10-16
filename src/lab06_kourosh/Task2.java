1a6a7159914ba0b3396468ef33ad6199d2664069
Kourosh Maissamyazad <2440557@edu.vaniercollege.qc.ca>
2025-10-16 08:21:00 -0400

    Task 2 finished

--- src/lab06_kourosh/Task2.java
+++ src/lab06_kourosh/Task2.java
@@ -8,6 +8,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -34,19 +36,53 @@ public class Task2 extends Application{
         porch.setFill(Color.BEIGE);
         
         Rectangle house = new Rectangle(SCENE_WIDTH/4, SCENE_HEIGHT - (SCENE_HEIGHT * 0.45), SCENE_WIDTH/2, SCENE_HEIGHT * 0.3);
-        house.setStroke(Color.LIGHTGRAY);
+        house.setStroke(Color.BLACK);
         house.setFill(Color.LIGHTGRAY);
         
-        Polygon roof = new Polygon(SCENE_WIDTH/4, SCENE_HEIGHT - (SCENE_HEIGHT * 0.45), SCENE_WIDTH * 0.75, SCENE_HEIGHT - (SCENE_HEIGHT * 0.45), SCENE_WIDTH/2, SCENE_HEIGHT * 0.3);
+        Polygon roof = new Polygon(SCENE_WIDTH/4, SCENE_HEIGHT - (SCENE_HEIGHT * 0.45), SCENE_WIDTH * 0.75, SCENE_HEIGHT - (SCENE_HEIGHT * 0.45), SCENE_WIDTH/2, SCENE_HEIGHT * 0.33);
         roof.setStroke(Color.RED);
         roof.setFill(Color.RED);
         
-        Rectangle door = new Rectangle(SCENE_WIDTH * 0.6, SCENE_HEIGHT * 0.65, SCENE_WIDTH * 0.1, SCENE_HEIGHT * 0.1);
+        Rectangle door = new Rectangle(SCENE_WIDTH * 0.5, SCENE_HEIGHT * 0.7, SCENE_WIDTH * 0.1, SCENE_HEIGHT * 0.15);
         door.setStroke(Color.BROWN);
         door.setFill(Color.BROWN);
         
+        Rectangle leftWindow = new Rectangle(house.getX() + 15, house.getY() + 35, SCENE_WIDTH * 0.1, SCENE_WIDTH * 0.1);
+        leftWindow.setStroke(Color.LIGHTBLUE);
+        leftWindow.setFill(Color.LIGHTBLUE);
+        
+        Line leftWindowVline = new Line(leftWindow.getX() + (leftWindow.getWidth()/2), leftWindow.getY(), leftWindow.getX() + (leftWindow.getWidth()/2), leftWindow.getY() + leftWindow.getHeight());
+        Line leftWindowHline = new Line(leftWindow.getX(), leftWindow.getY() + (leftWindow.getHeight()/2), leftWindow.getX() + leftWindow.getWidth(), leftWindow.getY() +  (leftWindow.getHeight()/2));
+        
+        Rectangle rightWindow = new Rectangle(house.getX() + house.getWidth() - (leftWindow.getWidth() + 15), leftWindow.getY(), leftWindow.getWidth(), leftWindow.getHeight());
+        rightWindow.setStroke(Color.LIGHTBLUE);
+        rightWindow.setFill(Color.LIGHTBLUE);
+        
+        Line rightWindowVline = new Line(rightWindow.getX() + rightWindow.getWidth()/2, rightWindow.getY(), rightWindow.getX() + rightWindow.getWidth()/2, rightWindow.getY() + rightWindow.getHeight());
+        Line rightWindowHline = new Line(rightWindow.getX(), rightWindow.getY() + rightWindow.getHeight()/2, rightWindow.getX() + rightWindow.getWidth(), rightWindow.getY() + rightWindow.getHeight()/2);
+        
+        Rectangle chimney = new Rectangle(door.getX() - 20, SCENE_HEIGHT * 0.37, SCENE_WIDTH * 0.04, SCENE_HEIGHT * 0.14 );
+        chimney.setStroke(Color.DARKGRAY);
+        chimney.setFill(Color.DARKGRAY);
+        
+        Circle sun = new Circle(SCENE_WIDTH - SCENE_WIDTH * 0.08 - 5, SCENE_WIDTH * 0.08 + 5, SCENE_WIDTH * 0.08);
+        sun.setStroke(Color.YELLOW);
+        sun.setFill(Color.YELLOW);
+        
+        Line sunLine1 = new Line(sun.getCenterX(), sun.getCenterY(), sun.getCenterX() - sun.getRadius() - 5, sun.getCenterY() - sun.getRadius());
+        sunLine1.setFill(Color.YELLOW);
+        sunLine1.setStroke(Color.YELLOW);
+        
+        Line sunLine2 = new Line(sun.getCenterX(), sun.getCenterY(), sunLine1.getEndX(), sunLine1.getEndY() + sun.getRadius() * 2);
+        sunLine2.setFill(Color.YELLOW);
+        sunLine2.setStroke(Color.YELLOW);
+        
+        Line sunLine3 = new Line(sun.getCenterX(), sun.getCenterY() + sun.getRadius(), SCENE_WIDTH, sun.getCenterY() + sun.getRadius());
+        sunLine3.setFill(Color.YELLOW);
+        sunLine3.setStroke(Color.YELLOW);
+        
         Pane root = new Pane();
-        root.getChildren().addAll(grass, porch, house, roof, door);
+        root.getChildren().addAll(grass, porch, house, roof, door, leftWindow, rightWindow, leftWindowVline, leftWindowHline, rightWindowVline, rightWindowHline, chimney, sun, sunLine1, sunLine2, sunLine3);
         
         Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
         stage.setScene(scene);
